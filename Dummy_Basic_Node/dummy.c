9827ae29372371572819f1b8c3b805a230129bf0
paul5566 <eric20021232000@gmail.com>
2019-07-08 10:35:15 -0700

    Add the probe and reomve function to make platform driver work
    Remove code:
    
    Add code:
    	-Add code to Implement the `platform_driver`
    	(1)`probe`
    	(2)`remove`
    
    Detailed Notes:
            - Problem :
            - RootCause :Platform Device Driver Not Work
            - Author : Paul
            - Build :
            (1)export ARCH=arm
            (2)export CROSS_COMPILE=gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-
            (3)make
    
    Testing Preformed:
    	- How to Test : minicom and UART

--- Dummy_Basic_Node/dummy.c
+++ Dummy_Basic_Node/dummy.c
@@ -50,31 +50,96 @@ MODULE_LICENSE("GPL");
 
 #define DRIVER_NAME	"dummy_testing"
 
-struct riowd {
+struct dummy {
 	void __iomem		*regs;
 	spinlock_t		lock;
 };
+
+static struct dummy *dummy_device;
+
 static int dummy_open(struct inode *inode, struct file *filp)
 {
 	nonseekable_open(inode, filp);
+	pr_info("Dummy Open\n");
 	return 0;
 }
 
 static int dummy_release(struct inode *inode, struct file *filp)
 {
+	pr_info("Dummy Release->END\n");
 	return 0;
 }
-static const struct file_operations riowd_fops = {
+static const struct file_operations dummy_fops = {
 	.owner =		THIS_MODULE,
 	.open =			dummy_open,
 	.release =		dummy_release,
 };
 
+static struct miscdevice dummy_miscdev = {
+	.minor	= WATCHDOG_MINOR,
+	.name	= "watchdog",
+	.fops	= &dummy_fops
+};
+
+static int dummy_probe(struct platform_device *op)
+{
+
+
+	struct dummy *p;
+	int err = -EINVAL;
+	
+	err = -ENOMEM;
+	p = devm_kzalloc(&op->dev, sizeof(*p), GFP_KERNEL);
+	if (!p)
+		goto out;
+
+
+	if (!p->regs) {
+		pr_err("Cannot map registers\n");
+		goto out;
+	}
+
+	/* Make miscdev useable right away */
+	dummy_device = p;
+/*
+ *
+ *	This is not correctly!!!! Think about the type of platform device
+ *	driver is different. 
+ *
+ *
+ * */
+	err = misc_register(&dummy_miscdev);
+	if (err) {
+		pr_err("Cannot register watchdog misc device\n");
+		goto out_iounmap;
+	}
+
+
+	platform_set_drvdata(op, p);
+	return 0;
+
+out_iounmap:
+	dummy_device = NULL;
+
+out:
+	return err;
+}
+
+static int dummy_remove(struct platform_device *op)
+{
+	struct dummy *p = platform_get_drvdata(op);
+
+	misc_deregister(&dummy_miscdev);
+
+	return 0;
+}
 
 static struct platform_driver dummy_driver = {
 	.driver = {
 		.name = DRIVER_NAME,
 	},
+	.probe		= dummy_probe,
+	.remove		= dummy_remove,
 };
 
 module_platform_driver(dummy_driver);
