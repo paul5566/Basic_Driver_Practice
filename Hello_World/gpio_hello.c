a8b426babc3c1a64513e71ee913a9d1ee9b4cd7d
paul5566 <eric20021232000@gmail.com>
2019-08-07 08:24:48 -0700

    Modified the code to make dynamic_eg.c more kernel style
    Add Code:
    (1)gpio_hello.c
    	*request gpio
    	*gpio_direction_output
            *gpio_set_value
    (2)4_9_Makefile:
    	Linux Kernel 4.9.14
    (3)4_14_Makfile
    	Linux Kernel 4.14.78
    
    Note:
    	Be careful about the line is used.
    	That's because of the the line has been occupied.
    	Using the new feature "lsgpio" to check
    
    	[ 3131.019266] start of the module
    	[ 3131.019298] Failed to request GPIO 137, error -16

--- Hello_World/gpio_hello.c
+++ Hello_World/gpio_hello.c
@@ -12,12 +12,82 @@
 
 
 
+
+#include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/sysctl.h>
+#include <linux/proc_fs.h>
+#include <linux/gpio.h>
+#include <linux/of_platform.h>
+#include <linux/of_gpio.h>
+#include <linux/mutex.h>
+#include <linux/delay.h>
+
+
+static DEFINE_MUTEX(gpio_lock);
+
+
+/*
+struct gpio_edm_data {
+		unsigned int gpio;
+		unsigned int value;
+		const char * direction;
+		const char *desc;
+};
+*/
+/*
+ truct platform_device {
+	const char	*name;
+	int		id;
+	bool		id_auto;
+	struct device	dev;
+	u32		num_resources;
+	struct resource	*resource;
+
+	const struct platform_device_id	*id_entry;
+	char *driver_override; // Driver name to force a match
+
+	// MFD cell pointer
+	struct mfd_cell *mfd_cell;
+
+	// arch specific additions
+	struct pdev_archdata	archdata;
+};
+ */
+
+
+
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int __init gpio_hello_init(void)
 {
-	pr_info("start of the module\n");
-	return 0;
+	int error;
+
+
+	if ( gpio_is_valid(137) ){
+			mutex_lock(&gpio_lock);
+			pr_info("start of the module\n");
+			error = gpio_request(137,"gpio-led");
+			if (error < 0) {
+				pr_info("Failed to request GPIO %d, error %d\n",137, error);
+				return error;
+			}
+			gpio_direction_output(137,true);
+			gpio_set_value(137, true);
+			msleep(15);
+			mutex_unlock(&gpio_lock);
+			return 0;
+	}
+	else
+		goto fail;
+
+//failed issue
+fail:
+	if (gpio_is_valid(137))
+		gpio_free(137);
+
+	return error;
+
 }
 
 
